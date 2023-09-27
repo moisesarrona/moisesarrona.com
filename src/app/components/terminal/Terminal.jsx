4755b2aff3860c041b4bde0abf67a7ad114a839f
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-09-26 23:56:04 -0600

    Open File
    Add Open files and comments in functions

--- src/app/components/terminal/Terminal.jsx
+++ src/app/components/terminal/Terminal.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
-import { commands, sections, terminalName } from './terminalData'
+import { commands, sections, terminalName, contents } from './terminalData'
 import TerminalInput from './TerminalInput';
 import TerminalHistory from './TerminalHistory';
+import TerminalInfo from './TerminalInfo'
 
 const Terminal = () => {
   const [input, setInput] = useState('')
   const [history, setHistory] = useState([])
   const [active, setActive] = useState('home')
 
+  /**
+   * Get input event
+   * @param {Object} e - Input event
+   */
   const inputSend = (e) => {
     if (e.key == 'Enter') {
       const inputValid = input.trim().replace(/\s{2,}/g, ' ')
@@ -17,6 +22,10 @@ const Terminal = () => {
     }
   }
 
+  /**
+   * Valid if input contain command
+   * @param {string} input - Input content
+   */
   const validateCommand = (input) => {
     if (input !== '') {
       const inputWords = input.split(' ');
@@ -27,6 +36,11 @@ const Terminal = () => {
     }
   }
 
+  /**
+   * Redirect function to excute commands
+   * @param {string} firstWord Input commanddd
+   * @param {string} secondWord Input param
+   */
   const executeCommand = (firstWord, secondWord) => {
     switch (firstWord) {
       case 'ls':
@@ -34,11 +48,11 @@ const Terminal = () => {
         break;
 
       case 'cd':
-        lsCd(secondWord)
+        catCd(firstWord, secondWord)
         break;
 
       case 'cat':
-        lsCd(secondWord)
+        catCd(firstWord, secondWord)
         break;
 
       case 'clear':
@@ -51,13 +65,17 @@ const Terminal = () => {
     }
   }
 
+  /**
+   * function to ls command
+   * @param {string} secondWord - Navigate
+   */
   const ls = (secondWord) => {
     if (secondWord === undefined)
-      sections.forEach((section, index) => {
+      sections.forEach((section) => {
         if (section[active]) {    
           const files = section[active].files;
           const folders = section[active].folders;
-          const all = [...files, ...folders].join(' ');
+          const all = [...files, ...folders];
           setHistory((prevHistory) => [...prevHistory, all]);
         }
       });
@@ -65,47 +83,83 @@ const Terminal = () => {
       msgError(secondWord, 'it is not a attribute')
   }
 
-  const lsCd = (secondWord) => {
-    if (secondWord !== undefined) {
-      const isFile = secondWord.includes('.txt')
-      if (!isFile) {
-        let msg = 'it is not a folder'
-        if (secondWord !== '...') {
-          sections.forEach((section, index) => {
-            if (section[active]) {
-              const folders = section[active].folders;
-              folders.includes(secondWord)? setActive(secondWord) : msgError(secondWord, msg)
-            }
-          })
-        } else {
-          setActive('home')
-        }
-      } else {
-        let msg = 'it is not a file'
-        sections.forEach((section, index) => {
-          if (section[active]) {
-            const files = section[active].files;
-            files.includes(secondWord)? console.log('hiiiii') : msgError(secondWord, msg)
-          }
-        })
-      }
-    } else  {
+  /**
+   * function to cat and cd command
+   * @param {string} secondWord - Folder or File
+   */
+  const catCd = (firstWord, secondWord) => {
+
+    if (secondWord === undefined) {
       msgError(secondWord, 'it is not a attribute')
+      return
     }
+
+    const isCd = firstWord === 'cd'
+    const isCat = firstWord === 'cat'
+    const isFile = secondWord.includes('.txt')
+
+    if (isCd && secondWord === '...') {
+      setActive('home')
+      return
+    }
+
+    const msg = isCat? 'it is not a file' : 'it is not a foler'
+
+    sections.forEach((section) => {
+      if (section[active]) {
+        const target = isFile? section[active].files : section[active].folders
+        if (target.includes(secondWord)) {
+          if (isCat && isFile) {
+            openFile(secondWord)
+
+          } else if (isCd && !isFile) {
+            setActive(secondWord)
+
+          } else {
+            msgError(secondWord, msg)
+
+          }
+        } else {
+          msgError(secondWord, msg)
+        }
+      }
+    })
   }
 
+  /**
+   * Clear history and input
+   */
   const clear = () => {
     setHistory([]);
     setInput('');
   }
 
+  /**
+   * Build error message to commands
+   * @param {string} word 
+   * @param {string} msg 
+   */
   const msgError = (word, msg) => {
     setHistory((prevHistory) => [...prevHistory, `${word} ${msg}`]);
   }
+
+  /**
+   * open content file
+   * @param {string} file 
+   */
+  const openFile = (file) => {
+    const matchingContent = contents.find((contentItem) => contentItem[active]
+    .some(item => item[file.split('.')[0]] !== undefined));
+    const content = matchingContent[active].find(item => item[file.split('.')[0]] !== undefined);
+    matchingContent? setHistory((prevHistory) => [...prevHistory, content[file.split('.')[0]]]) : msgError(file, 'Error in show content')
+  }
   
   return (
+    
     <>
         <div className="terminal">
+            <TerminalInfo />
+            
             <TerminalHistory 
               history={ history } 
               terminalName={ terminalName } />
--- src/app/components/terminal/terminalData.js
+++ src/app/components/terminal/terminalData.js
@@ -9,17 +9,35 @@ export const commands = [
 export const sections = [
   {
     home: {
+      id: 1,
       files: ['readme.txt'],
       folders: ['about', 'contact']
     },
     about: {
+      id: 2,
       files: ['readme.txt', 'portfolio.txt'],
       folders: []
     },
     contact: {
+      id: 3,
       files: ['contact.txt'],
       folers: []
     }
+  }
+]
+
+export const contents = [
+  {
+    home: [
+      {readme: 'Heeeeeeey, my name is Moises Arrona, Im Software Engineer and Software Architect'}
+    ],
+    about: [
+      {readme: 'Well, lest go'},
+      {portfolio: 'Ok, my skill are ...'}
+    ],
+    contact: [
+      {contact: 'My contact is ...'}
+    ]
   }
 ]
 
