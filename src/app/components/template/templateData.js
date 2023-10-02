b77133aca9c68cd215f96e228779c0eca5dad54d
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-10-01 22:58:16 -0600

    Add Project

--- src/app/components/template/templateData.js
+++ src/app/components/template/templateData.js
@@ -5,13 +5,15 @@ export const ITEM_PAGES = [
         active: true
     },
     {
+        name: "projects",
+        link: "/projects",
+        active: true
+    },
+    {
         name: "about",
         link: "/about",
         active: true
     },
-    {
-        name: "contact",
-        link: "/contact",
-        active: false
-    }
 ]
+
+export const VERSION_PROJECT = "v0.0.0";
