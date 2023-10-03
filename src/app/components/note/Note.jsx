479c2a084b3ca0c34451ba4f204e8fc7db5b58b6
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-10-02 22:44:43 -0600

    Add Note
    Add all logic to NoteComponent

--- src/app/components/note/Note.jsx
+++ src/app/components/note/Note.jsx
@@ -1,14 +1,50 @@
-import React from 'react'
-
-const text = "This web site has many secrets, discover them"
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { TIPS } from './noteData'
 
 const Note = () => {
+  const [tip, setTip] = useState('')
+  const [tipIndex, setTipIndex] = useState(0)
+  const path = useLocation().pathname.replace('/', '')
+  
+  /**
+   * Change text to page.
+   */
+  useEffect(() => {
+    selectText()
+
+    const interval = setInterval(() => {
+      changeIndex()
+    }, 10000);
+
+    return () => clearInterval(interval)
+  }, [path, tipIndex])
+
+  /**
+   * Select text to show
+   */
+  const selectText = () => {
+    const texts = TIPS[0][path]
+    texts.length > 0? setTip(texts[tipIndex]) : console.log('Error in select tip')
+  }
+
+  /**
+   * Change index to chnage text
+   */
+  const changeIndex = () => {
+    setTipIndex(prevIndex => (prevIndex + 1) % TIPS[0][path].length);
+  }
+
   return (
     <>
         <div className="note">
-            <p>TIP: [</p>
-            <p>{ text }</p>
-            <p>]</p>
+            <div className='note__header'>console.log(</div>
+            <div className='note__body'>
+                <span className='note__start'>"</span> { tip } <span className='note__start'>"</span>
+            </div>
+            <div className='note__footer'>
+              );
+            </div>
         </div>
     </>
   )
---
+++ src/app/components/note/noteData.js
@@ -0,0 +1,22 @@
+export const TIPS = [
+    {
+        home : [
+            "This website has many secret, discovered them",
+            "Ps ps ps, I've a terminal for u, use it",
+            "Oh, I'm Moises and this is my portfolio",
+            "I made it with React and love, I hope u like it."
+        ],
+        about: [
+            "Scroll down to discover more about me...",
+            "Do you want to know my skills? Lets gooo",
+            "I'm Software Architect. I love writing code 👨🏻‍💻 and playing the guitar 🎸",
+            "Java, SpringBoot, PHP, Laravel, Angular, React, Git, SqlServer, Mysql, AWS etc."
+        ],
+        projects: [
+            "I've some projects on Github, discover them",
+            "Arron EDI, Arron Privacy, moisesarrona.com, brisApp, and more",
+            "In my free time I maintain and create Open-Source projects for the community",
+            "In my job create Closed-Source projects,"
+        ]
+    }
+]
