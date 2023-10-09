da8132d58cec3642010849622086ce31a88b6599
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-10-09 00:52:31 -0600

    Change base styles
    Delete scroll bar, add fixed elements

--- src/app/landing/Home.jsx
+++ src/app/landing/Home.jsx
@@ -4,15 +4,15 @@ import img from '../../assets/img/moy.png'
 const Home = () => {
   return (
     <>
-        <section className='home'>
-            <div className='title'>
-                <h2>hey</h2>
-            </div>
+      <section>
+        <div className='home__title'>
+          <h1>hey</h1>
+        </div>
 
-            <div className='img'>
+        <div className='img'>
                 <img src={img} alt="" />
             </div>
-        </section>
+      </section>
     </>
   )
 }
