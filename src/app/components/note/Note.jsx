f1be22531cc67986c7c9df223127e94f6b74226f
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-11-30 23:36:43 -0600

    Add Note animaations

--- src/app/components/note/Note.jsx
+++ src/app/components/note/Note.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { TIPS } from '../../core/data/noteData'
-import { useRoutePath } from '../../context/RoutePathContext'
+import React, { useEffect, useRef, useState } from 'react';
+import { TIPS } from '../../core/data/noteData';
+import { useRoutePath } from '../../context/RoutePathContext';
+import gsap from 'gsap';
+import { useLoader } from '../../context/LoadContext';
+
 
 const Note = () => {
-  const [tip, setTip] = useState('')
+  const [tip, setTip] = useState('');
   const [tipIndex, setTipIndex] = useState(0)
   const currentPath = useRoutePath();
   const tipValue = TIPS[0][currentPath];
+  const { loaderFinished } = useLoader();
+  const contentAnimRef = useRef();
+  const timeLine = gsap.timeline();
 
   /**
    * Change text to page.
@@ -26,6 +32,33 @@ const Note = () => {
   }, [currentPath, tipIndex])
 
   /**
+   * Start animations with gsap when loaded page
+   */
+  useEffect(() => {
+    
+    /**
+     * Container animation
+     */
+    const contentAnimation = () => {
+      if(contentAnimRef.current) {
+        contentAnimRef.current.style.opacity = 0;
+        contentAnimRef.current.style.top = '150px';
+        if (loaderFinished) {
+          timeLine.to(contentAnimRef.current, {
+            opacity: 1,
+            top: '120px',
+            duration: 0.8
+          })
+        }
+      }
+    }
+
+    contentAnimation();
+  
+  }, [loaderFinished])
+  
+
+  /**
    * Select text to show
    */
   const selectText = () => {
@@ -42,7 +75,7 @@ const Note = () => {
 
   return (
     <>
-      <div className="note glass">
+      <div className="note glass" ref={contentAnimRef}>
         <div className='note__header'>console.log(</div>
         <div className='note__body'>
           <span className='note__start'>"</span> {tip} <span className='note__start'>"</span>
