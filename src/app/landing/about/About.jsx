e2dee1d0b17aee4f0a6bc405f173d7d379ba29d7
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-12-01 01:04:26 -0600

    Add About animation

--- src/app/landing/about/About.jsx
+++ src/app/landing/about/About.jsx
@@ -1,13 +1,72 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faInstagram, faLinkedinIn, faCodepen } from '@fortawesome/free-brands-svg-icons'
-import { ABOUT, SKILLS, STUDIES, CONTACT } from '../core/data/aboutData'
+import React, { useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram, faLinkedinIn, faCodepen } from '@fortawesome/free-brands-svg-icons';
+import { ABOUT, SKILLS, STUDIES, CONTACT } from '../../core/data/aboutData';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import { useLoader } from '../../context/LoadContext';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
+  const { loaderFinished } = useLoader();
+  const contentAnimRef = useRef();
+  const percentRefs = SKILLS.map(() => useRef());
+  const timeLine = gsap.timeline();
+
+  /**
+   * Start animations with gsap when loaded page
+   */
+  useEffect(() => {
+
+    /**
+     * Container animation
+     */
+    const contentAnimation = () => {
+      if (contentAnimRef.current) {
+        contentAnimRef.current.style.opacity = 0;
+        contentAnimRef.current.style.top = '50px';
+
+        if (loaderFinished) {
+          timeLine.to(contentAnimRef.current, {
+            opacity: 1,
+            top: 0,
+            duration: 0.8
+          })
+
+        }
+      }
+    }
+
+    /**
+     * Percent skill animation
+     */
+    const percentAnimation = () => {
+      SKILLS.forEach((skill, index) => {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: percentRefs[index].current,
+            toggleActions: 'restart'
+          },
+        });
+  
+        tl.from(percentRefs[index].current, {
+          width: 0,
+          duration: 1.3,
+          ease: "bounce.out"
+        });
+      });
+    };
+
+    contentAnimation();
+    percentAnimation();
+
+  }, [loaderFinished])
+
   return (
     <section>
-      <div className='content__about'>
+      <div className='content__about glass' ref={contentAnimRef}>
         <article>
           <div className='spetial__content'>
             <h1 className='spetial__title'><span className='spetial__letter'>W</span>ho I'm?</h1>
@@ -32,7 +91,7 @@ const About = () => {
                   <div className='skill' key={index}>
                     <div className='skill__name'>{skill.name}</div>
                     <div className='skill__line'>
-                      <div className='skill__color' style={{ width: skill.percent }}>
+                      <div className='skill__color' style={{ width: skill.percent }} ref={percentRefs[index]}>
                         <span className='skill__percent'>{skill.percent}</span>
                       </div>
                     </div>
@@ -84,8 +143,8 @@ const About = () => {
 
                         const resolvedIcon = link.name === "instagram" ? faInstagram :
                           link.name === "linkedin" ? faLinkedinIn :
-                            link.name === "correo" ? faEnvelope : 
-                            link.name === "codepen"? faCodepen : null;
+                            link.name === "correo" ? faEnvelope :
+                              link.name === "codepen" ? faCodepen : null;
                         const typeLink = link.name === "correo" ? "mailto:" : ""
 
                         return (
